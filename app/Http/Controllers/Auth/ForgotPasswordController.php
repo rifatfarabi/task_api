9d7a6acb1c3c500b912218c237400d04542c3a20
Md. Abdullah al farabi <45206269+rifatfarabi@users.noreply.github.com>
2022-11-03 09:41:33 +0600

    Forgot password added

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,19 +4,24 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
 
     use SendsPasswordResetEmails;
+
+    protected function sendResetLinkResponse(Request $request, $response){
+        return response()->json(['message' => trans($response)], Response::HTTP_OK);
+    }
+
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return response()->json([
+            "errors" => [
+                'email' => [trans($response)]
+            ]
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
 }
